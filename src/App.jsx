bf268e669963f14a8282fb102b9a7760b0669ac8
Samima Akter Tithy <tithysamima789@gmail.com>
2025-09-07 13:30:31 -0600

    Start the category section.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
-// import Category from "./components/Category/Category";
+import Category from "./components/Category/Category";
 // import Category2 from "./components/Category/Category2";
 // import Services from "./components/Services/Services";
 // import Banner from "./components/Banner/Banner";
@@ -62,7 +62,8 @@ const App = () => {
     <div className="bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-hidden">
       <Navbar />
       <Hero handleOrderPopup={handleOrderPopup} />
-      {/* <Category />
+      <Category />
+      {/*
       <Category2 />
       <Services />
       <Banner data={BannerData} />
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -68,7 +68,7 @@ const Hero = () => {
 
                                 <div className='order-1 sm:order-2'>
                                     <div>
-                                        <img src={data.img} alt='' className='w-[300px] h-[300px] sm:h-[450px] sm:scale-105 lg:scale-110 object-contain mx-auto drop-shadow-[-8px_4px_6px_rgba(0,0,0,0.4)] relative z-40'/>
+                                        <img src={data.img} alt='' className='w-[300px] h-[300px] sm:w-[450px] sm:h-[450px] sm:scale-105 lg:scale-110 object-contain mx-auto drop-shadow-[-8px_4px_6px_rgba(0,0,0,0.4)] relative z-40'/>
                                     </div>
                                 </div>
                             </div>
